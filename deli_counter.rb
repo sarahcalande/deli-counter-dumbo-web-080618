508ede0f906c1de99e60f4c4b429f7de28ca23cb
scalande@yahoo.com <scalande@yahoo.com>
2018-07-22 14:50:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,4 +9,8 @@ def line(katz_deli)
     end
     
     
-    
+    def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
