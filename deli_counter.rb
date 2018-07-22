12e29813ce6fc5b451d8b894ad07b707c75c9a45
scalande@yahoo.com <scalande@yahoo.com>
2018-07-22 14:51:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,8 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
+end
+
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
